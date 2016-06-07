7127e6186e3204e8d3e4d262c059b1e9e9fb99c5
lynnerw <larwitter@gmail.com>
2016-06-07 18:40:56 -0400

    round 1

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+var fizbuzNum = 0;
+  console.log(fizbuzNum);
+
+for (var fizbuzNum = 1; fizbuzNum < 11; fizbuzNum++) {
+  if (fizbuzNum === 5) {
+    document.write("what's up 5"); }
+    else {
+      document.write("<p>" + fizbuzNum + "</p>");
+    }
+};
+});
