4f7c82a4dbc4d70fd525a196245da15c72ba2c43
lynnerw <larwitter@gmail.com>
2016-06-07 18:57:04 -0400

    round 2

--- js/app.js
+++ js/app.js
@@ -3,10 +3,16 @@ $(document).ready(function() {
 var fizbuzNum = 0;
   console.log(fizbuzNum);
 
-for (var fizbuzNum = 1; fizbuzNum < 11; fizbuzNum++) {
-  if (fizbuzNum === 5) {
-    document.write("what's up 5"); }
-    else {
+for (var fizbuzNum = 1; fizbuzNum < 21; fizbuzNum++) {
+  if ((fizbuzNum % 3) === 0) {
+    document.write("<p>" + "fizz" +"</p>"); }
+    else
+      if ((fizbuzNum % 5) === 0) {
+        document.write("<p>" + "buzz" +"</p>"); }
+        else
+          if ((fizbuzNum % 3) + (fizbuzNum % 5) === 0) {
+            document.write("<p>" + "fizz buzz" +"</p>"); }
+            else {
       document.write("<p>" + fizbuzNum + "</p>");
     }
 };
