c7f7636a62043b9a78f820d89b4bf6abe943f97e
lynnerw <larwitter@gmail.com>
2016-06-07 20:09:44 -0400

    grand finale

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
 
-var fizbuzNum = 0;
-  console.log(fizbuzNum);
+document.write("<h1>" + "Fizzing and buzzing..." + "</h1>");
 
-for (var fizbuzNum = 1; fizbuzNum < 21; fizbuzNum++) {
-  if ((fizbuzNum % 3) === 0) {
-    document.write("<p>" + "fizz" +"</p>"); }
+for (var fizbuzNum = 1; fizbuzNum < 101; fizbuzNum++) {
+  if ((fizbuzNum % 3) + (fizbuzNum % 5) === 0) {
+    document.write("<p>" + "fizz buzz" + "</p>"); }
     else
-      if ((fizbuzNum % 5) === 0) {
-        document.write("<p>" + "buzz" +"</p>"); }
+      if ((fizbuzNum % 3) === 0) {
+        document.write("<p>" + "fizz" + "</p>"); }
         else
-          if ((fizbuzNum % 3) + (fizbuzNum % 5) === 0) {
-            document.write("<p>" + "fizz buzz" +"</p>"); }
+          if ((fizbuzNum % 5) === 0) {
+            document.write("<p>" + "buzz" + "</p>"); }
             else {
-      document.write("<p>" + fizbuzNum + "</p>");
-    }
+              document.write("<p>" + fizbuzNum + "</p>");
+              }
 };
+
 });
