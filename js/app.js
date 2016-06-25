a583ea3af21664fd01f481c26b46b7f9db4c9fb8
Mostafa <m.elsheikh11791@gmail.com>
2016-06-25 17:40:58 +0200

    minor refactor

--- js/app.js
+++ js/app.js
@@ -1,35 +1,36 @@
 $(document).ready(function() {
 
-document.write('<h1>' + 'Fizzing and buzzing...' + '</h1>');
+  document.write('<h1>' + 'Fizzing and buzzing...' + '</h1>');
 
-function fizzurNum(userNum) {
-
-for (var fizbuzNum = 1; fizbuzNum <= userNum; fizbuzNum++) {
-  if ((fizbuzNum % 3) + (fizbuzNum % 5) === 0) {
-    document.write('<p>' + 'fizz buzz' + '</p>'); }
-    else
-      if ((fizbuzNum % 3) === 0) {
-        document.write('<p>' + 'fizz' + '</p>'); }
-        else
+  function fizzurNum(userNum) {
+    for (var fizbuzNum = 1; fizbuzNum <= userNum; fizbuzNum++) {
+      if ((fizbuzNum % 3) + (fizbuzNum % 5) === 0) {
+        document.write('<p>' + 'fizz buzz' + '</p>');
+      } else {
+        if ((fizbuzNum % 3) === 0) {
+          document.write('<p>' + 'fizz' + '</p>');
+        } else {
           if ((fizbuzNum % 5) === 0) {
-            document.write('<p>' + 'buzz' + '</p>'); }
-            else {
-              document.write('<p>' + fizbuzNum + '</p>');
-              }
-  };
-}
-
-var userNum = prompt('Please enter a whole number greater than zero.', 90);
-
-while (isNaN(parseInt(userNum))) {
-  document.write('Ooops. I need a whole number. Try again...' + '<br>');
-  var userNum = prompt('Please enter a whole number greater than zero.', 90);
+            document.write('<p>' + 'buzz' + '</p>');
+          } else {
+            document.write('<p>' + fizbuzNum + '</p>');
+          }
+        }
+      }
+    }
   }
 
-  if ((parseInt(userNum) % parseInt(userNum)) === 0) {
-        document.write('You picked ' + (parseInt(userNum)) + '.<br>' + 'Here we go!<br>');
-        fizzurNum(userNum);
-        document.write('<br>' + 'That\s it; we\'re all done.');
-      };
+  var userNum = prompt('Please enter a whole number greater than zero.', 90);
+
+  while (isNaN(parseInt(userNum))) {
+    document.write('Ooops. I need a whole number. Try again...' + '<br>');
+    userNum = prompt('Please enter a whole number greater than zero.', 90);
+  }
+
+  var intUserNum = parseInt(userNum);
+
+  document.write('You picked ' + intUserNum + '.<br>' + 'Here we go!<br>');
+  fizzurNum(intUserNum);
+  document.write('<br>' + 'That\s it; we\'re all done.');
 
 });
